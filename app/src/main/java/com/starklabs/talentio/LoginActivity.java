23401e82df52b95dbf5cea2a63c5d4e8e007464a
Stark <35381380+Stark-Tony@users.noreply.github.com>
2020-03-13 00:42:27 +0530

    Authentication Initials

--- app/src/main/java/com/starklabs/talentio/LoginActivity.java
+++ app/src/main/java/com/starklabs/talentio/LoginActivity.java
@@ -1,15 +1,26 @@
 package com.starklabs.talentio;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -18,27 +29,87 @@ public class LoginActivity extends AppCompatActivity {
     String color3="#ffcdd2";
     LinearLayout mLinearLayout;
     TextInputEditText user,pass;
-    MaterialButton login;
+    MaterialButton bt_login;
+    TextView tv_signup;
+    ProgressBar mProgressBar;
+
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mFirebaseAuth=FirebaseAuth.getInstance();
+
+        mProgressBar=findViewById(R.id.login_progress_bar);
         mLinearLayout=findViewById(R.id.loginTopLayout);
         user=findViewById(R.id.username);
         pass=findViewById(R.id.password);
-        login=findViewById(R.id.button_login);
-
+        bt_login=findViewById(R.id.button_login);
+        tv_signup=findViewById(R.id.tv_signup);
 
         GradientDrawable mGradientDrawable=new GradientDrawable(GradientDrawable.Orientation.TR_BL,
                 new int[]{Color.parseColor(color1),Color.parseColor(color2),Color.parseColor(color3)});
         mLinearLayout.setBackgroundDrawable(mGradientDrawable);
 
-        login.setOnClickListener(new View.OnClickListener() {
+        bt_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                userlogin();
+            }
+        });
 
+        tv_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void userlogin() {
+        String email=user.getText().toString().trim();
+        String password=pass.getText().toString().trim();
+        if(email.isEmpty())
+        {
+            user.setError("E-mail is required");
+            user.requestFocus();
+            return;
+        }
+        else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            user.setError("Please enter valid E-mail");
+            user.requestFocus();
+            return;
+        }
+        if(password.isEmpty())
+        {
+            pass.setError("Password can't be blank");
+            pass.requestFocus();
+            return;
+        }
+        else if(password.length()<6)
+        {
+            pass.setError("Minimum password length is 6");
+            pass.requestFocus();
+            return;
+        }
+        mProgressBar.setVisibility(View.VISIBLE);
+        mFirebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                mProgressBar.setVisibility(View.GONE);
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(LoginActivity.this,"Successful",Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this,task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
---
+++ app/src/main/java/com/starklabs/talentio/SignupActivity.java
@@ -0,0 +1,116 @@
+package com.starklabs.talentio;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+import java.util.regex.Pattern;
+
+public class SignupActivity extends AppCompatActivity {
+
+    TextInputEditText signup_email,signup_pass;
+    MaterialButton signup_submit;
+    TextView signup_signin;
+    private FirebaseAuth mAuth;
+    ProgressBar mProgressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mProgressBar=findViewById(R.id.signup_progress_bar);
+        signup_email=findViewById(R.id.signup_email);
+        signup_pass=findViewById(R.id.signup_password);
+        signup_submit=findViewById(R.id.signup_submit);
+        signup_signin=findViewById(R.id.tv_signin);
+
+        signup_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
+
+        signup_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(SignupActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void registerUser() {
+        String email=signup_email.getText().toString().trim();
+        String password=signup_pass.getText().toString().trim();
+        if(email.isEmpty())
+        {
+            signup_email.setError("E-mail is required");
+            signup_email.requestFocus();
+            return;
+        }
+        else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            signup_email.setError("Please enter valid E-mail");
+            signup_email.requestFocus();
+            return;
+        }
+        if(password.isEmpty())
+        {
+            signup_pass.setError("Password can't be blank");
+            signup_pass.requestFocus();
+            return;
+        }
+        else if(password.length()<6)
+        {
+            signup_pass.setError("Minimum password length is 6");
+            signup_pass.requestFocus();
+            return;
+        }
+        mProgressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        mProgressBar.setVisibility(View.GONE);
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(SignupActivity.this,"Successfully Registered",Toast.LENGTH_LONG).show();
+                            Intent intent= new Intent(SignupActivity.this,LoginActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else
+                        {
+
+                            if(task.getException() instanceof FirebaseAuthUserCollisionException)
+                            {
+                                signup_email.setError("Already registered");
+                            }
+                            else
+                            {
+                                Toast.makeText(SignupActivity.this,"Error while registering",Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                });
+    }
+}
