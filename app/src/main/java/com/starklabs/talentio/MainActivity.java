59824e90c8b68d70febbbcf06b7bd86a7551cc19
Stark <35381380+Stark-Tony@users.noreply.github.com>
2020-03-21 12:58:12 +0530

    before final commit

--- app/src/main/java/com/starklabs/talentio/MainActivity.java
+++ app/src/main/java/com/starklabs/talentio/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AlphaAnimation;
@@ -18,6 +22,7 @@ import android.view.animation.Animation;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -26,6 +31,7 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -33,6 +39,7 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -54,6 +61,8 @@ public class MainActivity extends AppCompatActivity {
     FirebaseUser mFirebaseUser;
     TextView id_tv;
     FirebaseFirestore db;
+    NavigationView mNavigationView;
+    DialogInterface.OnClickListener dialogClickListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
         citySpinner=findViewById(R.id.spinner_city);
         instrSpinner=findViewById(R.id.spinner_instr);
         id_tv=findViewById(R.id.main_id_tv);
+        mNavigationView=findViewById(R.id.main_navigation_view);
 
         city=new ArrayList<>();
         instr=new ArrayList<>();
@@ -75,6 +85,21 @@ public class MainActivity extends AppCompatActivity {
         mToggle= new ActionBarDrawerToggle(myActivity,mDrawerLayout,mToolbar,R.string.open_drawer,R.string.close_drawer);
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
+
+
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Log.d("menu:",item.getItemId()+"");
+                switch (item.getItemId()) {
+                    case R.id.menu_logout:
+                        logoutMethod();
+                        break;
+                }
+                return true;
+            }
+        });
 
         mainLayout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -128,9 +153,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-
-                Intent intent = new Intent(MainActivity.this,RateActivity.class);
-                startActivity(intent);
+                if(citySpinner.getSelectedItem().equals("City") || instrSpinner.getSelectedItem().equals("Instructor"))
+                {
+                    Toast.makeText(myActivity, "Select Appropriate Values", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    String selectedCity=(String) citySpinner.getSelectedItem();
+                    String selectedInstr=(String) instrSpinner.getSelectedItem();
+                    String cityId= searchCity(selectedCity);
+                    String instrId=searchInstr(selectedInstr);
+                    Intent intent = new Intent(MainActivity.this,RateActivity.class);
+                    intent.putExtra("cityId",cityId);
+                    intent.putExtra("instrId",instrId);
+                    intent.putExtra("instrName",selectedInstr);
+                    startActivity(intent);
+                }
             }
         });
 
@@ -142,6 +180,8 @@ public class MainActivity extends AppCompatActivity {
             id_tv.append("   ");
             id_tv.append(mFirebaseUser.getEmail());
         }
+
+
 
         citySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -156,10 +196,10 @@ public class MainActivity extends AppCompatActivity {
                 tempAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 instrSpinner.setAdapter(tempAdapter);
 
-                if(id!=0)
+                if(position!=0)
                 {
                     try {
-                        Log.d("selected:",(String)parent.getItemAtPosition(position));
+                        Log.d("selected:",(String)parent.getItemAtPosition(position)+":"+position+":"+city.get(position-1).get(0));
                         getInstructor(city.get(position-1).get(0));
                     }catch(Exception e)
                     {
@@ -174,6 +214,47 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    private void logoutMethod() {
+
+        FirebaseAuth mAuth=FirebaseAuth.getInstance();
+        FirebaseUser user=mAuth.getCurrentUser();
+        if(user!=null)
+        {
+            mAuth.signOut();
+            Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+            startActivity(intent);
+            Toast.makeText(myActivity, "Signed out successfully", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    private String searchInstr(String s) {
+        String retId=null;
+        for(int i=0;i<instr.size();i++)
+        {
+            if(instr.get(i).get(1).equals(s))
+            {
+                retId=instr.get(i).get(0);
+                return retId;
+            }
+        }
+        return retId;
+    }
+
+    private String searchCity(String s) {
+        String retId=null;
+        for(int i=0;i<city.size();i++)
+        {
+            if(city.get(i).get(1).equals(s))
+            {
+                retId=city.get(i).get(0);
+                return retId;
+            }
+        }
+        return retId;
     }
 
 
@@ -196,12 +277,6 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean getInstructor(String city)
     {
-        ProgressDialog mProgressDialog;
-        mProgressDialog= new ProgressDialog(MainActivity.this);
-        mProgressDialog.setCancelable(false);
-        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        mProgressDialog.setMessage("Loading Cities...");
-        mProgressDialog.show();
 
         Log.d("city:",city);
         instrList=new ArrayList<>();
@@ -216,11 +291,17 @@ public class MainActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful() && task.getResult()!=null) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d("GETINFO", document.getId() + " => " + document.getData());
+                                //Log.d("GETINFO", document.getId() + " => " + document.getData());
                                 ArrayList<String> temp=new ArrayList<>();
-                                temp.add(document.getId());
-                                temp.add((String) document.get("name"));
-                                instr.add(temp);
+                                try {
+                                    temp.add(document.getId());
+                                    temp.add((String) document.get("name"));
+                                    instr.add(temp);
+                                }catch(Exception e)
+                                {
+                                    Toast.makeText(myActivity, "Couldn't complete operation\n Contact admin", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
                             }
 
                             for(int i=0;i<instr.size();i++)
@@ -235,23 +316,17 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-
+        Collections.sort(instrList);
+        instrSpinner.setAdapter(null);
         instrAdapter= new ArrayAdapter<>(MainActivity.this,android.R.layout.simple_spinner_item,instrList);
         instrAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         instrSpinner.setAdapter(instrAdapter);
 
-        mProgressDialog.cancel();
         return true;
     }
 
     private boolean getCities()
     {
-        ProgressDialog mProgressDialog;
-        mProgressDialog= new ProgressDialog(MainActivity.this);
-        mProgressDialog.setCancelable(false);
-        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        mProgressDialog.setMessage("Loading Cities...");
-        mProgressDialog.show();
 
         cityList= new ArrayList<>();
         cityList.add("City");
@@ -289,12 +364,59 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
+        Collections.sort(cityList);
         cityAdapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_spinner_item,cityList);
         cityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         citySpinner.setAdapter(cityAdapter);
 
-        mProgressDialog.cancel();
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(mToggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        dialogClickListener= new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        finish();
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        dialog.dismiss();
+                        break;
+                }
+            }
+        };
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            MenuItem item = mNavigationView.getCheckedItem();
+            if (item != null)
+                item.setChecked(false);
+            else
+            {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("Do you want to exit?")
+                        .setPositiveButton("Yes",dialogClickListener)
+                        .setNegativeButton("No",dialogClickListener)
+                        .setCancelable(false)
+                        .show();
+
+
+            }
+            super.onBackPressed();
+        }
+
     }
 
 }
--- app/src/main/java/com/starklabs/talentio/RateActivity.java
+++ app/src/main/java/com/starklabs/talentio/RateActivity.java
@@ -1,12 +1,29 @@
 package com.starklabs.talentio;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.RatingBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Document;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RateActivity extends AppCompatActivity
 {
@@ -14,7 +31,12 @@ public class RateActivity extends AppCompatActivity
 
     RatingBar bar1,bar2,bar3,bar4,bar5;
     MaterialButton submit;
-
+    String cityId;
+    String instrId;
+    String instrName;
+    double v1,v2,v3,v4,v5;
+    double num;
+    ProgressDialog localProgress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,12 +49,99 @@ public class RateActivity extends AppCompatActivity
         bar4 = findViewById(R.id.rating_bar_4);
         bar5 = findViewById(R.id.rating_bar_5);
 
+        localProgress=new ProgressDialog(RateActivity.this);
+        localProgress.setCancelable(false);
+        localProgress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        localProgress.setMessage("Submitting...");
+
+        cityId=getIntent().getStringExtra("cityId");
+        instrId=getIntent().getStringExtra("instrId");
+        instrName=getIntent().getStringExtra("instrName");
+
+        Log.d("instr:",cityId+":"+instrId);
+
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                double b1,b2,b3,b4,b5;
+                b1=bar1.getRating();
+                b2=bar2.getRating();
+                b3=bar3.getRating();
+                b4=bar4.getRating();
+                b5=bar5.getRating();
 
+                localProgress.show();
+                saveRating(b1,b2,b3,b4,b5);
             }
         });
+
+    }
+
+    private void saveRating(final double b1, final double b2, final double b3, final double b4, final double b5) {
+        FirebaseFirestore db= FirebaseFirestore.getInstance();
+        db.collection("cities").document(cityId).collection("rating")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful() && task.getResult()!=null)
+                        {
+                            for(QueryDocumentSnapshot document: task.getResult())
+                            {
+                                if(document.getId().equals(instrId))
+                                {
+                                    try{
+                                        v1= (double) document.get("p1");
+                                        v2= (double) document.get("p2");
+                                        v3= (double) document.get("p3");
+                                        v4= (double) document.get("p4");
+                                        v5= (double) document.get("p5");
+                                        num= (double) document.get("total");
+
+                                        Log.d("mydata:",v1+":"+v2+":"+v3+":"+v4+":"+v5+":"+num);
+
+                                        double temp1=((v1*num)+(b1))/(num+1);
+                                        double temp2=((v2*num)+(b2))/(num+1);
+                                        double temp3=((v3*num)+(b3))/(num+1);
+                                        double temp4=((v4*num)+(b4))/(num+1);
+                                        double temp5=((v5*num)+(b5))/(num+1);
+                                        double numTemp= num+1.0;
+                                        Log.d("mydata:",temp1+":"+temp2+":"+temp3+":"+temp4+":"+temp5+":"+numTemp);
+
+                                        saveData(numTemp, temp1, temp2, temp3, temp4, temp5);
+                                    }catch(Exception e)
+                                    {
+                                        Toast.makeText(RateActivity.this, "Something went wrong\nContact Admin", Toast.LENGTH_LONG).show();
+                                        Log.d("mydata:",e.getMessage());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void saveData(double numTemp, double temp1, double temp2, double temp3, double temp4, double temp5) {
+
+        FirebaseFirestore db= FirebaseFirestore.getInstance();
+        db.collection("cities").document(cityId).collection("rating")
+                .document(instrId)
+                .update("name",instrName,"total",numTemp,"p1",temp1,"p2",temp2,"p3",temp3,"p4",temp4,"p5",temp5)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(RateActivity.this, "Rated Successfully", Toast.LENGTH_SHORT).show();
+                            localProgress.cancel();
+                            finish();
+                        }
+                        else
+                        {
+                            Toast.makeText(RateActivity.this, "Couldn't rate at this moment", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
     }
 
